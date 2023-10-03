9847527ed9642928a6d0955f6a9a6e94607282eb
dndeveloper-saurabh <saurabh@pustack.com>
2023-10-03 10:31:31 +0530

    fix: fast scroll fis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import AppStore from '../assets/images/app-store.svg';
 import GooglePlay from '../assets/images/google-play.svg';
 import Image from 'next/image';
 import {LinearRateOptions} from '@/helpers/canvas';
+import {Simulate} from 'react-dom/test-utils';
+import reset = Simulate.reset;
 
 function getLinearRateNew(options: LinearRateOptions) {
   const x = (options.desiredRange.from - options.desiredRange.to) / (options.relativeRange.from - options.relativeRange.to);
@@ -44,8 +46,10 @@ export default function Home() {
 
     // let i = 0;
 
+    let start
 
-    if(latestValue < 1700) {
+
+    if(latestValue < 1400) {
       let a ={
         bg: 1,
         title: 1,
@@ -59,25 +63,25 @@ export default function Home() {
       // });
       a.title = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1024, to: 1100},
+        relativeRange: {from: 536, to: 950},
         acc: latestValue
       });
       a.para = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1140, to: 1200},
+        relativeRange: {from: 930, to: 1000},
         acc: latestValue
       });
 
-      if(latestValue > 1440) {
+      if(latestValue > 1350) {
         a.title = a.para = getLinearRateNew({
           desiredRange: {from: 0.9999, to: 0},
-          relativeRange: {from: 1440, to: 1600},
+          relativeRange: {from: 1350, to: 1400},
           acc: latestValue
         });
 
         a.bg = getLinearRateNew({
           desiredRange: {from: 0.9999, to: 0},
-          relativeRange: {from: 1600, to: 1700},
+          relativeRange: {from: 1350, to: 1400},
           acc: latestValue
         });
       }
@@ -86,12 +90,12 @@ export default function Home() {
 
       setVisibilityObject(c => ({
         a,
-        b: {...c.b},
+        b: resetObj,
         c: resetObj,
         d: resetObj
       }))
     }
-    if(latestValue > 1536 && latestValue < 1536 + 1024) {
+    if(latestValue > 1400 && latestValue < 2100) {
       let b = {
         bg: 1,
         title: 1,
@@ -100,45 +104,45 @@ export default function Home() {
       }
       b.bg = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1700, to: 1800},
+        relativeRange: {from: 1400, to: 1450},
         acc: latestValue
       });
       b.title = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1800, to: 1910},
+        relativeRange: {from: 1400, to: 1450},
         acc: latestValue
       });
       b.para = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1950, to: 2030},
+        relativeRange: {from: 1440, to: 1500},
         acc: latestValue
       });
 
 
-      if(latestValue > 2270) {
+      if(latestValue > 1850) {
         b.title = b.para = getLinearRateNew({
           desiredRange: {from: 0.9999, to: 0},
-          relativeRange: {from: 2270, to: 2400},
+          relativeRange: {from: 1850, to: 1900},
           acc: latestValue
         });
 
         b.bg = getLinearRateNew({
           desiredRange: {from: 0.9999, to: 0},
-          relativeRange: {from: 2400, to: 2560},
+          relativeRange: {from: 1850, to: 1900},
           acc: latestValue
         });
       }
 
 
       setVisibilityObject(c => ({
-        a: {...c.a},
+        a: resetObj,
         b,
         c: resetObj,
         d: resetObj
       }))
     }
 
-    if(latestValue > 1536 + 1024 - 200 && latestValue < 1536 + 1024 + 1024) {
+    if(latestValue > 1900 && latestValue < 1536 + 1024 + 1024) {
       let c = {
         bg: 1,
         title: 1,
@@ -147,31 +151,31 @@ export default function Home() {
       }
       c.bg = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1700 + 1024 - 200, to: 1800 + 1024 - 200},
+        relativeRange: {from: 1900, to: 1950},
         acc: latestValue
       });
       c.title = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1800 + 1024 - 200, to: 1910 + 1024 - 200},
+        relativeRange: {from: 1900, to: 1950},
         acc: latestValue
       });
       c.para = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1950 + 1024 - 200, to: 2030 + 1024 - 200},
+        relativeRange: {from: 1940, to: 2000},
         acc: latestValue
       });
 
 
-      if(latestValue > 2270 + 1024 - 200) {
+      if(latestValue > 2350) {
         c.title = c.para = getLinearRateNew({
           desiredRange: {from: 0.9999, to: 0},
-          relativeRange: {from: 2270 + 1024 - 200, to: 2400 + 1024 - 200},
+          relativeRange: {from: 2350, to: 2400},
           acc: latestValue
         });
 
         c.bg = getLinearRateNew({
           desiredRange: {from: 0.9999, to: 0},
-          relativeRange: {from: 2400 + 1024 - 200, to: 2560 + 1024},
+          relativeRange: {from: 2350, to: 2400},
           acc: latestValue
         });
       }
@@ -179,13 +183,13 @@ export default function Home() {
 
       setVisibilityObject(_c => ({
         a: resetObj,
-        b: {..._c.b},
+        b: resetObj,
         c,
-        d: {..._c.d}
+        d: resetObj
       }))
     }
 
-    if(latestValue > 1536 + 1024 + 1024 - 200 && latestValue < 1536 + 1024 + 1024 + 1024) {
+    if(latestValue > 2400 && latestValue < 1536 + 1024 + 1024 + 1024) {
       let d = {
         bg: 1,
         title: 1,
@@ -194,17 +198,17 @@ export default function Home() {
       }
       d.bg = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1700 + 1024 + 1024 - 200, to: 1800 + 1024 + 1024 - 200},
+        relativeRange: {from: 2400, to: 2450},
         acc: latestValue
       });
       d.title = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1800 + 1024 + 1024 - 200, to: 1910 + 1024 + 1024 - 200},
+        relativeRange: {from: 2400, to: 2450},
         acc: latestValue
       });
       d.para = getLinearRateNew({
         desiredRange: {from: 0, to: 0.9999},
-        relativeRange: {from: 1950 + 1024 + 1024 - 200, to: 2030 + 1024 + 1024 - 200},
+        relativeRange: {from: 2440, to: 2500},
         acc: latestValue
       });
 
@@ -227,7 +231,7 @@ export default function Home() {
       setVisibilityObject(_c => ({
         a: resetObj,
         b: resetObj,
-        c: {..._c.c},
+        c: resetObj,
         d
       }))
     }
@@ -294,7 +298,7 @@ export default function Home() {
   return (
     <main>
       <div className="w-screen h-screen bg-white" />
-      <div className="w-screen h-[500vh]">
+      <div className="w-screen h-[350vh]">
         <div className="sticky top-0 left-0 w-screen h-[100vh]">
           <div className="absolute w-screen h-screen top-0 flex justify-between items-center left-0 z-50">
             <div style={{opacity: visibilityObject.a.bg}} className="absolute top-1/2 left-1/2 transform rounded-full -translate-x-1/2 -translate-y-1/2 home-bg w-[80%] h-[80%]" />
